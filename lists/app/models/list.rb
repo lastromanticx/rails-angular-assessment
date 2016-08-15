649ac0b03676be86ceb8dca99b8967d2f3c01ac7
lastromanticx <giladbarkan@live.com>
2016-08-15 15:37:49 -0400

    added collaborators= method to list class to add users based on permission and set their permissions

--- lists/app/models/list.rb
+++ lists/app/models/list.rb
@@ -10,4 +10,17 @@ class List < ApplicationRecord
   def permission(user)
     user_lists.where(user_id: user.id).first.permission
   end
+
+  def collaborators=(emails)
+    collaborator_list = emails.gsub(","," ").split(/\s+/).map{|email| User.find_by(email: email)}.compact
+    creator = user_lists.where(permission: "creator").first.user 
+    collaborator_list << creator
+    self.users.clear
+    collaborator_list.uniq.each do |u| 
+      self.users << u
+      update_permission(u, "collaborator")
+    end
+    update_permission(creator, "creator")
+    save
+  end
 end
