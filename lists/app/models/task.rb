fbb84a33cfb7e5b61755805b022b6abda30322a3
lastromanticx <giladbarkan@live.com>
2016-08-15 15:34:26 -0400

    added date-string formatting and serializer method including all tags for angular task forms to task class

--- lists/app/models/task.rb
+++ lists/app/models/task.rb
@@ -7,4 +7,20 @@ class Task < ApplicationRecord
     tag = Tag.find_or_create_by(tag_attributes.values[0]) if tag_attributes.values[0]["name"].match(/[a-zA-Z]/)
     tags << tag if tag and not tags.include?(tag)
   end
+
+  def due_date_string
+    due_date.to_s
+  end
+
+  def serialize_with_all_tags
+    {
+      id: id,
+      name: name,
+      description: description,
+      due_date_string: due_date_string,
+      status: status,
+      tags: tags.map(&:id),
+      all_tags: Tag.all.map{|tag| {id: tag.id, name: tag.name}}
+    }
+  end
 end
