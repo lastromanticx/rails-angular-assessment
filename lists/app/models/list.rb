e8b06ffed7ba7e22bff686b99642dd70134ae25e
lastromanticx <giladbarkan@live.com>
2016-08-15 15:27:22 -0400

    added permission and update_permission methods to list class

--- lists/app/models/list.rb
+++ lists/app/models/list.rb
@@ -2,4 +2,12 @@ class List < ApplicationRecord
   has_many :user_lists
   has_many :users, through: :user_lists
   has_many :tasks
+
+  def update_permission(user,permission)
+    user_lists.where(user_id: user.id).first.update(permission: permission)
+  end
+
+  def permission(user)
+    user_lists.where(user_id: user.id).first.permission
+  end
 end
